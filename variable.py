007055666d64420ce130b49959bf179b0d3c74f5
Lotfy <az_lotfy@outlook.com>
2022-08-19 18:17:13 +0200

    commit 1

---
+++ variable.py
@@ -0,0 +1,12 @@
+# create variables
+# using f and variable
+# using str() and variable
+Name_Of_Unit = "Seconds"
+Calculation_To_Units = 24 * 60 * 60
+print(f"50 days = {20*24*60*60} {Name_Of_Unit} ")
+print(f"20 Days = {Calculation_To_Units * 20} {Name_Of_Unit}")
+print("20 Days = " + str(20*24*60*60) + " " + str(Name_Of_Unit))
+print("20 Days = " + str(Calculation_To_Units * 24 * 60 * 60) + " " + str(Name_Of_Unit))
+
+
+
